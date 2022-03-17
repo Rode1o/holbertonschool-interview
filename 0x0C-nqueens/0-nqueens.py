9b56d8343f64f46637b2b2f49fea23c5a46d9feb
Duvan R <dumirola.dr@gmail.com>
2022-03-17 11:14:54 -0500

    qeens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,44 +1,90 @@
 #!/usr/bin/python3
-""" N queens """
-import sys
 
 
-if len(sys.argv) > 2 or len(sys.argv) < 2:
+from sys import argv
+
+
+if len(argv) != 2:
     print("Usage: nqueens N")
     exit(1)
 
-if not sys.argv[1].isdigit():
+n_queens = argv[1]
+
+try:
+    n_queens = int(n_queens)
+except ValueError:
     print("N must be a number")
     exit(1)
 
-if int(sys.argv[1]) < 4:
+if n_queens < 4:
     print("N must be at least 4")
     exit(1)
 
-n = int(sys.argv[1])
+non_attacking_queens = []
+rows_attacked = []
+cols_attacked = []
 
 
-def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
-    if i < n:
-        for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
-    else:
-        yield a
+def is_safe(new_queen):
+    if new_queen[0] in cols_attacked:
+        return False
+    if new_queen[1] in rows_attacked:
+        return False
+
+    """ Checks diagonal up left """
+    i = new_queen[0]
+    j = new_queen[1]
+    while i >= 0 and j >= 0:
+        if [i, j] in non_attacking_queens:
+            return False
+
+        i -= 1
+        j -= 1
+
+    """ Checks diagonal up right """
+    i = new_queen[0]
+    j = new_queen[1]
+    while j < n_queens and i >= 0:
+        if [i, j] in non_attacking_queens:
+            return False
+
+        i -= 1
+        j += 1
+
+    return True
 
 
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
+def search_queens(col_start):
+    for i in range(n_queens):
+        if i != 0:
+            col_start = 0
+        for j in range(col_start, n_queens):
+            if [i, j] not in non_attacking_queens:
+                if is_safe([i, j]):
+                    non_attacking_queens.append([i, j])
+                    cols_attacked.append(i)
+                    rows_attacked.append(j)
+
+    return non_attacking_queens
 
 
-solve(n)
+def finds_all_possibilities():
+    col_start = 0
+
+    while True:
+        non_attacking_queens.clear()
+        rows_attacked.clear()
+        cols_attacked.clear()
+        possible_solution = search_queens(col_start)
+        """ print(possible_solution) """
+
+        if len(possible_solution) == n_queens:
+            print(possible_solution)
+
+        col_start += 1
+
+        if col_start == n_queens:
+            break
+
+
+finds_all_possibilities()
